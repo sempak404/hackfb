fec7d1f7ea96f5b870c4f1e94d37174932fb8cbe
sempak404 <54473839+sempak404@users.noreply.github.com>
2019-10-04 21:19:33 +0700

    Add files via upload

---
+++ hackfb.sh
@@ -0,0 +1,31 @@
+clear
+echo "===========================" | lolcat
+toilet -f slant --gay " HackFb"
+echo "===========================" | lolcat
+echo ""
+echo ""
+echo ""
+echo "Tunggu Sebentar"
+echo "==========================" | lolcat
+echo "==========================" | lolcat
+read -p "ID/NamaFb : " name
+echo "==========================" | lolcat
+echo "==========================" | lolcat
+echo ""
+echo ""
+echo ""
+echo "Sedang MengCrack"
+echo "1%"
+echo "3%"
+echo "9%"
+echo "20"
+echo "38%"
+echo "46%"
+echo "63%"
+echo "85%"
+echo "92%"
+echo "100%"
+echo "Selamat Katasandi Ketemu"
+echo "=============================="
+echo " Kata Sandi : gaming020612"
+echo "=============================="
